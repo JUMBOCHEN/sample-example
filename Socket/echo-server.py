cce37db889dc3dfc8e9b8fb77699e436e89d524e
JUMBOCHEN <641724352@qq.com>
2017-11-26 12:36:22 +0800

    增加注释

--- Socket/echo-server.py
+++ Socket/echo-server.py
@@ -1,10 +1,11 @@
+'''此处代码参看python cookbook3第12章'''
 from socket import*
-myHost = ''
+myHost = '' #此处为空字符串，默认服务器host为本机IP
 myPort = 50007
 
-sockobj = socket(AF_INET, SOCK_STREAM)
-sockobj.bind((myHost, myPort))
-sockobj.listen(5)
+sockobj = socket(AF_INET, SOCK_STREAM) #创建socket udp
+sockobj.bind((myHost, myPort)) #socket 绑定 host，port
+sockobj.listen(5) #开始监听到来的客户端连接，并允许保留5个挂起的请求
 
 while True:
 	connection, address = sockobj.accept()
