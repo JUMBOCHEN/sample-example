0846d9f748cf9a894d29bbfd5a3e80d404134626
JUMBOCHEN <641724352@qq.com>
2017-11-27 20:56:19 +0800

    找出目录最大.py文件

--- bigpy-dir.py
+++ bigpy-dir.py
@@ -1,13 +1,13 @@
 '''找出单个目录下最大的python源代码文件
 搜索window python源代码库，除非指定了dir命令行参数'''
 import os, sys, glob
-dirname = r'C:\Program Files\JetBrains\PyCharm 2017.2\lib' if len(sys.argv) == 1 else sys.argv[1]
+dirname = r'C:\Program Files (x86)\Python\Python36-32\Lib' if len(sys.argv) == 1 else sys.argv[1]
 
 allsizes = []
 allpy = glob.glob(dirname + os.sep + '*.py')
 for filename in allpy:
     filesize = os.path.getsize(filename)
-    allsizes.append(filesize, filename)
+    allsizes.append((filesize, filename))
 
 allsizes.sort()
 print(allsizes[:2])
