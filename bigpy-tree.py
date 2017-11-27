6cb800ffabb5983a6c432679ef683f6443dc11a1
JUMBOCHEN <641724352@qq.com>
2017-11-27 21:16:15 +0800

    找出目录最大.py文件(扫描标准库目录树)

---
+++ bigpy-tree.py
@@ -0,0 +1,18 @@
+'''找出整个目录树中最大的python源代码文件
+搜索源代码库，并利用pprint模块漂亮的打印出来'''
+import sys, os, pprint
+trace = False
+if sys.platform.startswith('win'):
+    dirname = r'C:\Program Files (x86)\Python\Python36-32\Lib' #windows platform
+else:
+    dirname = 'usr/lib/python' #linux,unix and cygwin platform
+
+allsizes = []
+for (thisDir, subsHere, filesHere) in os.walk(dirname):
+    if trace: print(thisDir)
+    for filename in filesHere:
+        if filename.endwith('.py'):
+            if trace: print('...',filename)
+            fullname = os.path.join(thisDir, filename)
+            fullsize = os.path.getsize(fullname)
+            allsizes.append((fullsize, fullname))
