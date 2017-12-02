05eeef66fcff87426c95a93c81dde8a879835838
JUMBOCHEN <641724352@qq.com>
2017-12-02 15:03:10 +0800

    扫描标准库目录树，找最大.py文件

--- FileTest/bigpy-tree.py
+++ FileTest/bigpy-tree.py
@@ -11,8 +11,12 @@ allsizes = []
 for (thisDir, subsHere, filesHere) in os.walk(dirname):
     if trace: print(thisDir)
     for filename in filesHere:
-        if filename.endwith('.py'):
-            if trace: print('...',filename)
+        if filename.endswith('.py'):
+            if trace: print('...', filename)
             fullname = os.path.join(thisDir, filename)
             fullsize = os.path.getsize(fullname)
             allsizes.append((fullsize, fullname))
+
+allsizes.sort()
+pprint.pprint(allsizes[:2])
+pprint.pprint(allsizes[-2:])
