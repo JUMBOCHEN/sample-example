2412713667a2fc46c81e388d3536853eb3f04b88
JUMBOCHEN <641724352@qq.com>
2017-12-19 23:17:02 +0800

    emmmmmm

--- selenium2/baidu.py
+++ selenium2/baidu.py
@@ -3,6 +3,10 @@ from selenium import webdriver
 
 driver = webdriver.Chrome() #从https://sites.google.com/a/chromium.org/chromedriver/downloads下载chromeDrive
 driver.get("http://www.baidu.com")
+text = driver.find_element_by_id("cp").text
+print(text)
 driver.find_element_by_id("kw").send_keys('selenium2')
 driver.find_element_by_id("su").click()
+for list1 in range(1, 10):
+    driver.refresh()
 driver.quit()
