8d151f7a3a67644fd1ce616aacd3aa3b685aed3c
JUMBOCHEN <641724352@qq.com>
2018-04-03 14:34:43 +0800

    websocket

---
+++ websocket/testApp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from dwebsocket.decorators import accept_websocket,require_websocket
+from django.http import HttpResponse
+
+
+@accept_websocket
+def echo(request):
+    if not request.is_websocket():#判断是不是websocket连接
+        try:#如果是普通的http方法
+            message = request.GET['message']
+            return HttpResponse(message)
+        except:
+            return render(request,'index.html')
+    else:
+        for message in request.websocket:
+            request.websocket.send(message)#发送消息到客户端
